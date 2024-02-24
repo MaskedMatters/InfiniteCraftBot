e921b0d25574015b8c1e38997801d08f2523d71f
MaskedMatters <Masked.Matters@gmail.com>
2024-02-24 15:44:01 -0500

    Make my code much better and add comments

--- server.js
+++ server.js
@@ -1,14 +1,45 @@
-function requestUrl(first, second) {
-    return 'https://neal.fun/api/infinite-craft/pair?first=' + first + '&second=' + second
+// AN ASYNC FUNCTION THAT CALLS THE INFINITE CRAFT API AND RETURNS THE RESPONSE (NOT PARSED)
+async function requestAPI(first, second) {
+    
+    // OPTIONS FOR FETCH REQUEST
+    const options = {
+        method: 'GET',                                        // REQUEST TYPE
+        headers: {
+            'Referer': 'https://neal.fun/infinite-craft/',    // PRETEND TO BE INFINITE CRAFT
+            'User-Agent': 'Solver'                            // PRETEND TO BE INFINITE CRAFT
+        }
+    }
+
+    // REQUEST AND RETURN (NOT PARSED) RESPONSE
+    const response = await fetch(`https://neal.fun/api/infinite-craft/pair?first=${first}&second=${second}`, options)
+    return await response
+
 }
 
-const response = fetch(requestUrl('Water', 'Ampharos 5.5'), {
-    headers: {
-        'Referer': 'https://neal.fun/infinite-craft/',
-        'User-Agent': 'Solver'
-    }
-})
+// INITIALIZE A SET WITH THE 4 STARTED ELEMENTS
+let elements = new Set(['Water', 'Fire', 'Wind', 'Earth'])
 
-response
-    .then((res) => res.json())
-    .then((data) => console.log(data))
+// AN ASYNC FUNCTION THAT GOES THROUGH EVERY COMBINATION OF ELEMENTS IN THE SET
+async function fetchData() {
+
+    // GRAB ONE ELEMENT FROM THE ELEMENT SET (IN ORDER)
+    for (let first of elements) {
+
+        // GRAB A SECOND ELEMENT FROM THE ELEMENT SET (IN ORDER)
+        for (let second of elements) {
+
+            const response = (await requestAPI(first, second)).json()                           // SEND API REQUEST TO INFINITE CRAFT
+
+            elements.add(response.result)                                                       // ADD THE NEW ELEMENT TO THE SET
+            console.log(`${first} and ${second} make ${response.emoji}  ${response.result}`)    // LOG THE NEW ELEMENT TO CONSOLE
+
+            if (response.isNew == true) {
+                console.log("OH MY GOD!! THE BOT WORKS!!!! FIRST DISCOVERY!!!!!")               // IF FIRST DISCOVERY, LOG
+                return                                                                          // IF FIRST DISCOVERY, RETURN
+            }
+        }
+    }
+}
+
+// START FINDING!!!
+fetchData()
